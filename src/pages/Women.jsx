e5709c268d8a253f2404dd138d1f8359d24c70a0
shalinekoh <kohshaline@gmail.com>
2024-09-11 00:21:14 +0800

    Add women's clothing component with API fetch and basic product listing

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,6 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
 function Women() {
+    const [women, setWomen] = useState()
+
+    useEffect(() => {
+        const fetchImage = async() => {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+                const data = await response.json();
+
+                setWomen(data)
+            } catch (error) {
+                console.error('Error fetching Women Clothing:', error);
+            }
+        };
+        fetchImage();
+    }, []);
+
+    const handleClick = () => {
+        console.log("clicked")
+    }
+
+    if (!women) return "loading";
+
     return (
-        <h1>THIS IS Women</h1>
+        <>
+            <h2>Women's Clothing</h2>
+            {women.map((res) => (
+                <div key={res.id} className="productCard">
+                    <img src={res.image}></img>
+                    <p>{res.title}</p>
+                    <p>${res.price}</p>
+                    <button type="button" onClick={handleClick}>Add to Cart</button>
+                </div>
+            ))
+            }
+        </>
     )
 }
 
