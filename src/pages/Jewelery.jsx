fa29fda4e3bc2802062fc454c5cd787ad25d2124
shalinekoh <kohshaline@gmail.com>
2024-09-11 00:27:17 +0800

    Add men's clothing and jewelery component with API fetch and basic product listing

--- src/pages/Jewelery.jsx
+++ src/pages/Jewelery.jsx
@@ -1,6 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
 function Jewelery() {
+    const [jewelery, setJewelery] = useState()
+
+    useEffect(() => {
+        const fetchImage = async() => {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products/category/jewelery")
+                const data = await response.json();
+
+                setJewelery(data)
+            } catch (error) {
+                console.error('Error fetching Jewelery:', error);
+            }
+        };
+        fetchImage();
+    }, []);
+
+    const handleClick = () => {
+        console.log("clicked")
+    }
+
+    if (!jewelery) return "loading";
+
     return (
-        <h1>THIS IS Jewelery</h1>
+        <>
+            <h2>Jewelery</h2>
+            {jewelery.map((res) => (
+                <div key={res.id} className="productCard">
+                    <img src={res.image}></img>
+                    <p>{res.title}</p>
+                    <p>${res.price}</p>
+                    <button type="button" onClick={handleClick}>Add to Cart</button>
+                </div>
+            ))
+            }
+        </>
     )
 }
 
--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -1,6 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
 function Men() {
+    const [men, setMen] = useState()
+
+    useEffect(() => {
+        const fetchImage = async() => {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products/category/men's%20clothing")
+                const data = await response.json();
+
+                setMen(data)
+            } catch (error) {
+                console.error('Error fetching Men Clothing:', error);
+            }
+        };
+        fetchImage();
+    }, []);
+
+    const handleClick = () => {
+        console.log("clicked")
+    }
+
+    if (!men) return "loading";
+
     return (
-        <h1>THIS IS Men</h1>
+        <>
+            <h2>Men's Clothing</h2>
+            {men.map((res) => (
+                <div key={res.id} className="productCard">
+                    <img src={res.image}></img>
+                    <p>{res.title}</p>
+                    <p>${res.price}</p>
+                    <button type="button" onClick={handleClick}>Add to Cart</button>
+                </div>
+            ))
+            }
+        </>
     )
 }
 
