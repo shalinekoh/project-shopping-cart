30b54677b25d146bfc9a45988217bb861f4e9d05
shalinekoh <kohshaline@gmail.com>
2024-09-13 02:19:09 +0800

    Add styles to cart page

--- src/pages/shoppingCart.jsx
+++ src/pages/shoppingCart.jsx
@@ -9,64 +9,92 @@ function ShoppingCart() {
         handleViewDetails,
         wishList,
         setWishList,
-        handleWishList, ] = useOutletContext();
+        handleWishList] = useOutletContext();
 
     const handleMinus = (id) => {
         const quantity = cart.find((item) => item.id === id).quantity - 1;
         setCart((currentCart) => {
-
-            if (quantity < 1){
-                return currentCart.filter((item) => item.id !== id)
+            if (quantity < 1) {
+                return currentCart.filter((item) => item.id !== id);
             } else {
                 return currentCart.map((item) => (
-                        item.id === id
-                        ? {...item, quantity: item.quantity - 1}
-                        : item));
+                    item.id === id
+                        ? { ...item, quantity: item.quantity - 1 }
+                        : item
+                ));
             }
-        })
+        });
     }
 
     const handlePlus = (id) => {
         const quantity = cart.find((item) => item.id === id).quantity + 1;
         setCart((currentCart) => {
-            if (quantity <= 10){
+            if (quantity <= 10) {
                 return currentCart.map((item) => (
-                        item.id === id
-                        ? {...item, quantity: item.quantity + 1}
-                        : item));
+                    item.id === id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                ));
             }
-        })
+        });
     }
 
     if (!cart) return "Loading...";
-    if (cart.length === 0) return "Nothing in cart";
+    if (cart.length === 0) return <p className="text-center text-gray-600">Nothing in cart</p>;
 
     return (
-        <div className="cart">
-            <h2>Shopping Cart</h2>
+        <div className="min-h-screen mt-24 bg-gray-100 p-6">
+            <h2 className="text-3xl font-bold mb-6 text-center">Shopping Cart</h2>
             {cart.map((item) => (
-                <div key={item.id} className="cartItem">
-                    <img src={item.imgUrl} />
-                    <h3>{item.title}</h3>
-                    <p>${Math.round(item.quantity * item.price*100)/100} </p>
-                    <div className="quantity">
-                        <button onClick={() => handleMinus(item.id)}>-</button>
-                        <input type="number" value={item.quantity} min="0" max="10"
-                        onChange={e=> e.target.value}
-                        />
-                        <button onClick={() => {handlePlus(item.id)}}>+</button>
+                <div key={item.id} className="bg-white md:w-1/2 mx-auto border rounded-lg shadow-md p-4 mb-4 flex items-center">
+                    <img
+                        src={item.imgUrl}
+                        alt={item.title}
+                        className="w-32 h-32 object-fit rounded-md mr-4"
+                    />
+                    <div className="flex-1 ">
+                        <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+                        <p className="text-lg font-bold text-gray-700 mb-2">${Math.round(item.quantity * item.price * 100) / 100}</p>
+                        <div className="flex items-center gap-2">
+                            <button
+                                onClick={() => handleMinus(item.id)}
+                                className="bg-gray-300 text-gray-700 py-1 px-3 rounded-md hover:bg-gray-400"
+                            >
+                                -
+                            </button>
+                            <input
+                                type="number"
+                                value={item.quantity}
+                                min="0"
+                                max="10"
+                                className="text-center border rounded-md w-12"
+                                onChange={e => e.target.value}
+                            />
+                            <button
+                                onClick={() => handlePlus(item.id)}
+                                className="bg-gray-300 text-gray-700 py-1 px-3 rounded-md hover:bg-gray-400"
+                            >
+                                +
+                            </button>
+                        </div>
                     </div>
-
                 </div>
-                ))
-            }
-            <div className="total">
-                <p>Total Items: {cart.reduce((acc, item) => acc + item.quantity , 0)}</p>
-                <p>Total Amount: ${Math.round(cart.reduce((acc, item) => acc + (item.quantity * item.price), 0) * 100) / 100}</p>
-                <button>CHECK OUT</button>
+            ))}
+            <div className="flex flex-col bg-white md:w-1/2 mx-auto border rounded-lg shadow-md p-4 mt-6">
+                <p className="flex text-lg font-semibold mb-2">ORDER SUMMARY | {cart.reduce((acc, item) => acc + item.quantity, 0)} ITEM(S)</p>
+
+                <div className="flex justify-between text-lg font-bold text-gray-700 mb-4">
+                    <span >Total Amount: </span>
+                    <span>${Math.round(cart.reduce((acc, item) => acc + (item.quantity * item.price), 0) * 100) / 100}</span>
+                </div>
+                <button
+                    className="btn"
+                >
+                    CHECK OUT
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
